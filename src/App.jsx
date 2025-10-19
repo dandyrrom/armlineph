dd895d5bf1b580dfd2f64b80e805e9fcbef22482
dandyrrom <dannicromo@gmail.com>
2025-10-19 22:40:01 +0800

    feat: Add automatic scroll to top on page navigation

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
+      <ScrollToTop />
       <Header />
 
       {/* This main section will now display our routed pages */}
